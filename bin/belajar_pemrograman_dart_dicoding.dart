8b4a84d3de2b27460cfea7874c0ad6c39623e231
maulanabin <2041720132@student.polinema.ac.id>
2022-08-08 20:23:51 +0700

    First commit

---
+++ bin/belajar_pemrograman_dart_dicoding.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_pemrograman_dart_dicoding/belajar_pemrograman_dart_dicoding.dart' as belajar_pemrograman_dart_dicoding;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_pemrograman_dart_dicoding.calculate()}!');
+}
